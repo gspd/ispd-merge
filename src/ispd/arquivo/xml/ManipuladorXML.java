88a9026e16b11430305b70a18b2e64fac966c053
Math_RPG <maugusilva.santos@gmail.com>
2022-10-06 19:03:31 -0300

    Refactor ManipuladorXML.

--- src/ispd/arquivo/xml/ManipuladorXML.java
+++ src/ispd/arquivo/xml/ManipuladorXML.java
@@ -20,69 +20,72 @@ import java.io.IOException;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
-/**
- * @author denison
- */
 public class ManipuladorXML {
+    public static Document ler(final File file, final String dtdPath)
+            throws ParserConfigurationException, IOException, SAXException {
 
-    public static Document ler(final File xmlFile, final String dtd) throws ParserConfigurationException, IOException, SAXException {
-        Document documento = null;
-        final DocumentBuilderFactory factory =
-                DocumentBuilderFactory.newInstance();
+        final var factory = ManipuladorXML.makeFactory();
+        final var builder = ManipuladorXML.makeDocBuilder(dtdPath, factory);
+        return builder.parse(file);
+    }
+
+    private static DocumentBuilderFactory makeFactory() {
+        final var factory = DocumentBuilderFactory.newInstance();
         factory.setNamespaceAware(true);
         factory.setValidating(true);
-        final DocumentBuilder builder = factory.newDocumentBuilder();
-        final String dtdT = dtd;
-        //Indicar local do arquivo .dtd
-        builder.setEntityResolver(new EntityResolver() {
-            final InputSource substitute =
-                    new InputSource(ManipuladorXML.class.getResourceAsStream(
-                            "dtd/" + dtdT));
+        return factory;
+    }
 
-            @Override
-            public InputSource resolveEntity(final String publicId,
-                                             final String systemId) throws SAXException, IOException {
-                return this.substitute;
-            }
-        });
-        documento = builder.parse(xmlFile);
-        return documento;
+    private static DocumentBuilder makeDocBuilder(
+            final String dtdPath, final DocumentBuilderFactory factory) throws ParserConfigurationException {
+        final var builder = factory.newDocumentBuilder();
+        builder.setEntityResolver(new SubstituteEntityResolver(dtdPath));
+        return builder;
     }
 
     /**
      * Este método sobrescreve ou cria arquivo xml
      */
-    public static boolean escrever(final Document documento,
-                                   final File arquivo,
-                                   final String doc_system,
-                                   final Boolean omitTagXML) {
+    public static boolean escrever(final Document doc,
+                                   final File outputFile,
+                                   final String docTypeSystem,
+                                   final Boolean omitXmlDecl) {
         try {
-            final TransformerFactory tf = TransformerFactory.newInstance();
-            //tf.setAttribute("indent-number", new Integer(4));
-            final Transformer transformer = tf.newTransformer();
-            final DOMSource source = new DOMSource(documento);
-            final StreamResult result = new StreamResult(arquivo);
-            transformer.setOutputProperty(OutputKeys.ENCODING, "ISO-8859-1");
-            transformer.setOutputProperty(OutputKeys.INDENT, "yes");
-            transformer.setOutputProperty("{http://xml.apache" +
-                                          ".org/xslt}indent-amount", "4");
-            transformer.setOutputProperty(OutputKeys.DOCTYPE_SYSTEM,
-                    doc_system);
-            if (omitTagXML) {
-                transformer.setOutputProperty(OutputKeys.OMIT_XML_DECLARATION
-                        , "yes");
-            }
+            final var transformer =
+                    ManipuladorXML.makeTransformer(docTypeSystem, omitXmlDecl);
+
+            final var source = new DOMSource(doc);
+            final var result = new StreamResult(outputFile);
+
             transformer.transform(source, result);
-        } catch (final TransformerConfigurationException ex) {
-            Logger.getLogger(ManipuladorXML.class.getName()).log(Level.SEVERE
-                    , null, ex);
-            return false;
+
+            return true;
         } catch (final TransformerException ex) {
-            Logger.getLogger(ManipuladorXML.class.getName()).log(Level.SEVERE
-                    , null, ex);
+            Logger.getLogger(ManipuladorXML.class.getName())
+                    .log(Level.SEVERE, null, ex);
+
             return false;
         }
-        return true;
+    }
+
+    private static Transformer makeTransformer(
+            final String docTypeSystem, final Boolean omitXmlDecl)
+            throws TransformerConfigurationException {
+        final var transformer =
+                TransformerFactory.newInstance().newTransformer();
+
+        transformer.setOutputProperty(OutputKeys.ENCODING, "ISO-8859-1");
+        transformer.setOutputProperty(OutputKeys.INDENT, "yes");
+        transformer.setOutputProperty(
+                "{http://xml.apache.org/xslt}indent-amount", "4");
+        transformer.setOutputProperty(OutputKeys.DOCTYPE_SYSTEM, docTypeSystem);
+
+        if (omitXmlDecl) {
+            transformer.setOutputProperty(
+                    OutputKeys.OMIT_XML_DECLARATION, "yes");
+        }
+
+        return transformer;
     }
 
     /**
@@ -92,15 +95,29 @@ public class ManipuladorXML {
      */
     public static Document novoDocumento() {
         try {
-            final DocumentBuilderFactory factory =
-                    DocumentBuilderFactory.newInstance();
-            final DocumentBuilder builder = factory.newDocumentBuilder();
+            final var factory = DocumentBuilderFactory.newInstance();
+            final var builder = factory.newDocumentBuilder();
             return builder.newDocument();
         } catch (final ParserConfigurationException ex) {
-            Logger.getLogger(ManipuladorXML.class.getName()).log(Level.SEVERE
-                    , null, ex);
-            return null;
+            Logger.getLogger(ManipuladorXML.class.getName())
+                    .log(Level.SEVERE, null, ex);
         }
+
+        return null;
     }
 
+    private static class SubstituteEntityResolver implements EntityResolver {
+        private final InputSource substitute;
+
+        private SubstituteEntityResolver(final String dtd) {
+            this.substitute = new InputSource(
+                    ManipuladorXML.class.getResourceAsStream("dtd/" + dtd));
+        }
+
+        @Override
+        public InputSource resolveEntity(
+                final String publicId, final String systemId) {
+            return this.substitute;
+        }
+    }
 }
