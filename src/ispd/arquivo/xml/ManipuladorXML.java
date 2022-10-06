3ffe5bf06dc8d4a7166dbe244793b7ce66522c03
Math_RPG <maugusilva.santos@gmail.com>
2022-10-06 18:54:22 -0300

    Reformat and code cleanup.

--- src/ispd/arquivo/xml/ManipuladorXML.java
+++ src/ispd/arquivo/xml/ManipuladorXML.java
@@ -1,49 +1,10 @@
-/* ==========================================================
- * iSPD : iconic Simulator of Parallel and Distributed System
- * ==========================================================
- *
- * (C) Copyright 2010-2014, by Grupo de pesquisas em Sistemas Paralelos e Distribuídos da Unesp (GSPD).
- *
- * Project Info:  http://gspd.dcce.ibilce.unesp.br/
- *
- * This program is free software; you can redistribute it and/or
- * modify it under the terms of the GNU General Public License
- * as published by the Free Software Foundation; either version 2
- * of the License, or (at your option) any later version.
- * 
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- * 
- * You should have received a copy of the GNU General Public License
- * along with this program; if not, write to the Free Software
- * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
- *
- * [Oracle and Java are registered trademarks of Oracle and/or its affiliates. 
- * Other names may be trademarks of their respective owners.]
- *
- * ---------------
- * ManipuladorXML.java
- * ---------------
- * (C) Copyright 2014, by Grupo de pesquisas em Sistemas Paralelos e Distribuídos da Unesp (GSPD).
- *
- * Original Author:  Denison Menezes (for GSPD);
- * Contributor(s):   -;
- *
- * Changes
- * -------
- * 
- * 09-Set-2014 : Version 2.0;
- * 16-Set-2014 : Salvando arquivo sem tag com versão do xml
- *
- */
 package ispd.arquivo.xml;
 
-import java.io.File;
-import java.io.IOException;
-import java.util.logging.Level;
-import java.util.logging.Logger;
+import org.w3c.dom.Document;
+import org.xml.sax.EntityResolver;
+import org.xml.sax.InputSource;
+import org.xml.sax.SAXException;
+
 import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
 import javax.xml.parsers.ParserConfigurationException;
@@ -55,32 +16,34 @@ import javax.xml.transform.TransformerFactory;
 import javax.xml.transform.dom.DOMResult;
 import javax.xml.transform.dom.DOMSource;
 import javax.xml.transform.stream.StreamResult;
-import org.w3c.dom.Document;
-import org.w3c.dom.Node;
-import org.xml.sax.EntityResolver;
-import org.xml.sax.InputSource;
-import org.xml.sax.SAXException;
+import java.io.File;
+import java.io.IOException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 /**
- *
  * @author denison
  */
 public class ManipuladorXML {
 
-    public static Document ler(File xmlFile, String dtd) throws ParserConfigurationException, IOException, SAXException {
+    public static Document ler(final File xmlFile, final String dtd) throws ParserConfigurationException, IOException, SAXException {
         Document documento = null;
-        DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
+        final DocumentBuilderFactory factory =
+                DocumentBuilderFactory.newInstance();
         factory.setNamespaceAware(true);
         factory.setValidating(true);
-        DocumentBuilder builder = factory.newDocumentBuilder();
+        final DocumentBuilder builder = factory.newDocumentBuilder();
         final String dtdT = dtd;
         //Indicar local do arquivo .dtd
         builder.setEntityResolver(new EntityResolver() {
-            InputSource substitute = new InputSource(ManipuladorXML.class.getResourceAsStream("dtd/"+dtdT));
+            final InputSource substitute =
+                    new InputSource(ManipuladorXML.class.getResourceAsStream(
+                            "dtd/" + dtdT));
 
             @Override
-            public InputSource resolveEntity(String publicId, String systemId) throws SAXException, IOException {
-                return substitute;
+            public InputSource resolveEntity(final String publicId,
+                                             final String systemId) throws SAXException, IOException {
+                return this.substitute;
             }
         });
         documento = builder.parse(xmlFile);
@@ -90,26 +53,34 @@ public class ManipuladorXML {
     /**
      * Este método sobrescreve ou cria arquivo xml
      */
-    public static boolean escrever(Document documento, File arquivo, String doc_system, Boolean omitTagXML) {
+    public static boolean escrever(final Document documento,
+                                   final File arquivo,
+                                   final String doc_system,
+                                   final Boolean omitTagXML) {
         try {
-            TransformerFactory tf = TransformerFactory.newInstance();
+            final TransformerFactory tf = TransformerFactory.newInstance();
             //tf.setAttribute("indent-number", new Integer(4));
-            Transformer transformer = tf.newTransformer();
-            DOMSource source = new DOMSource(documento);
-            StreamResult result = new StreamResult(arquivo);
+            final Transformer transformer = tf.newTransformer();
+            final DOMSource source = new DOMSource(documento);
+            final StreamResult result = new StreamResult(arquivo);
             transformer.setOutputProperty(OutputKeys.ENCODING, "ISO-8859-1");
             transformer.setOutputProperty(OutputKeys.INDENT, "yes");
-            transformer.setOutputProperty("{http://xml.apache.org/xslt}indent-amount", "4");
-            transformer.setOutputProperty(OutputKeys.DOCTYPE_SYSTEM, doc_system);
+            transformer.setOutputProperty("{http://xml.apache" +
+                                          ".org/xslt}indent-amount", "4");
+            transformer.setOutputProperty(OutputKeys.DOCTYPE_SYSTEM,
+                    doc_system);
             if (omitTagXML) {
-                transformer.setOutputProperty(OutputKeys.OMIT_XML_DECLARATION, "yes");
+                transformer.setOutputProperty(OutputKeys.OMIT_XML_DECLARATION
+                        , "yes");
             }
             transformer.transform(source, result);
-        } catch (TransformerConfigurationException ex) {
-            Logger.getLogger(ManipuladorXML.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (final TransformerConfigurationException ex) {
+            Logger.getLogger(ManipuladorXML.class.getName()).log(Level.SEVERE
+                    , null, ex);
             return false;
-        } catch (TransformerException ex) {
-            Logger.getLogger(ManipuladorXML.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (final TransformerException ex) {
+            Logger.getLogger(ManipuladorXML.class.getName()).log(Level.SEVERE
+                    , null, ex);
             return false;
         }
         return true;
@@ -122,41 +93,47 @@ public class ManipuladorXML {
      */
     public static Document novoDocumento() {
         try {
-            DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
-            DocumentBuilder builder = factory.newDocumentBuilder();
+            final DocumentBuilderFactory factory =
+                    DocumentBuilderFactory.newInstance();
+            final DocumentBuilder builder = factory.newDocumentBuilder();
             return builder.newDocument();
-        } catch (ParserConfigurationException ex) {
-            Logger.getLogger(ManipuladorXML.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (final ParserConfigurationException ex) {
+            Logger.getLogger(ManipuladorXML.class.getName()).log(Level.SEVERE
+                    , null, ex);
             return null;
         }
     }
-    
-    public static Document[] clone(Document doc, int number) throws TransformerConfigurationException, TransformerException {
-        Document[] documento = new Document[number];
-        TransformerFactory tfactory = TransformerFactory.newInstance();
-        Transformer tx = tfactory.newTransformer();
-        DOMSource source = new DOMSource(doc);
-        DOMResult result = new DOMResult();
+
+    public static Document[] clone(final Document doc, final int number) throws TransformerException {
+        final Document[] documento = new Document[number];
+        final TransformerFactory tfactory = TransformerFactory.newInstance();
+        final Transformer tx = tfactory.newTransformer();
+        final DOMSource source = new DOMSource(doc);
+        final DOMResult result = new DOMResult();
         tx.transform(source, result);
         for (int i = 0; i < number; i++) {
             documento[i] = (Document) result.getNode();
         }
         return documento;
     }
-    
-    public static Document[] clone(File file, int number) throws ParserConfigurationException, IOException, SAXException {
-        Document[] documento = new Document[number];
-        DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
+
+    public static Document[] clone(final File file, final int number) throws ParserConfigurationException, IOException, SAXException {
+        final Document[] documento = new Document[number];
+        final DocumentBuilderFactory factory =
+                DocumentBuilderFactory.newInstance();
         factory.setNamespaceAware(true);
         factory.setValidating(true);
-        DocumentBuilder builder = factory.newDocumentBuilder();
+        final DocumentBuilder builder = factory.newDocumentBuilder();
         //Indicar local do arquivo .dtd
         builder.setEntityResolver(new EntityResolver() {
-            InputSource substitute = new InputSource(IconicoXML.class.getResourceAsStream("iSPD.dtd"));
+            final InputSource substitute =
+                    new InputSource(IconicoXML.class.getResourceAsStream(
+                            "iSPD.dtd"));
 
             @Override
-            public InputSource resolveEntity(String publicId, String systemId) throws SAXException, IOException {
-                return substitute;
+            public InputSource resolveEntity(final String publicId,
+                                             final String systemId) throws SAXException, IOException {
+                return this.substitute;
             }
         });
         for (int i = 0; i < number; i++) {
@@ -165,5 +142,5 @@ public class ManipuladorXML {
         //inputStream.close();
         return documento;
     }
-    
+
 }
